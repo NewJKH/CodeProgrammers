55b083551ca79f8c064995e3e9290c4479fd0a5f
JKH <newsun0805@gmail.com>
2025-04-30 19:12:00 +0900

    [level 1] Title: 평균 구하기, Time: 1.47 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,12 +1,27 @@
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class Solution {
+    /*
+     class Wrapper {
+        int value = 0;
+    }
+    
+    public double solution(int[] arr) {
+        Wrapper sum = new Wrapper();
+
+        Arrays.stream(arr)
+                .forEach(value -> sum.value+=value);
+
+        return (double) sum.value / arr.length;
+    }
+    */
     public double solution(int[] arr) {
         AtomicInteger sum = new AtomicInteger();
 
         Arrays.stream(arr)
-              .forEach(sum::addAndGet);
+                .forEach(sum::addAndGet);
 
         return (double) sum.get() / arr.length;
     }
