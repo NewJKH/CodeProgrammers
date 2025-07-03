03ca98bf7e6dbb8d5af763c68174c03d586aa6cd
JKH <newsun0805@gmail.com>
2025-07-03 09:17:43 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.29 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 09:17:31
+2025년 07월 03일 09:17:40
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,32 +1,6 @@
-import java.util.Stack;
-
 class Solution {
     public int[] solution(String s) {
-        int originStr = s.length();
-        int zeroCount = 0;
-        int loopCount = 0;
-        while(s.length() > 1){
-            s = s.replace("0","");
-            zeroCount += originStr - s.length();
-            
-            // s 를 2진법
-            Stack<Integer> stack = new Stack<>();
-            int decimal = s.length();
-            while (decimal > 0) {
-              int remainder = decimal % 2;
-              stack.push(remainder);
-              decimal /= 2;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            while (!stack.isEmpty()) {
-              sb.append(stack.pop());
-            }
-
-            s = sb.toString();
-            originStr = s.length();
-            loopCount++;
-        }
-        return new int[]{loopCount,zeroCount};
-    }   
+        int[] answer = {};
+        return answer;
+    }
 }
