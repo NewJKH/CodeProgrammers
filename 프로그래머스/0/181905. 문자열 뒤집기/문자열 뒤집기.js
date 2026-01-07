5069f167c082e3b400348c2dc97d30e6667bbbd7
JKH <newsun0805@gmail.com>
2026-01-06 18:26:23 -0800

    [level 0] Title: 문자열 뒤집기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, s, e) {
+    let str = my_string.slice(s,e+1).split('').reverse().join('');
+    return my_string.slice(0,s) + 
+        str + 
+        my_string.slice(e+1);
+}
